01269a0ef5bfaa79dbdb576cc952587d01a57701
hyo321 <hyo321@lguplus.co.kr>
2022-09-09 11:06:46 +0900

    Initial commit

---
+++ src/main/kotlin/com/pado/selfgrowingadults/SelfGrowingAdultsApplication.kt
@@ -0,0 +1,11 @@
+package com.pado.selfgrowingadults
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SelfGrowingAdultsApplication
+
+fun main(args: Array<String>) {
+    runApplication<SelfGrowingAdultsApplication>(*args)
+}
---
+++ src/test/kotlin/com/pado/selfgrowingadults/SelfGrowingAdultsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pado.selfgrowingadults
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SelfGrowingAdultsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
