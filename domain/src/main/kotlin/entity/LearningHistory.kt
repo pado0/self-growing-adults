0b3bb170e58485a213d8a9adcd6ceec503bd9443
pado0 <bgshhd95@gmail.com>
2022-09-21 18:09:55 +0900

    memberProfileIds VO 추가, Embedded, embeddable 공부

--- domain/src/main/kotlin/entity/LearningHistory.kt
+++ domain/src/main/kotlin/entity/LearningHistory.kt
@@ -1,17 +1,23 @@
 package entity
 
 import constant.DB_NULL_ID
-import javax.persistence.Entity
-import javax.persistence.Id
+import vo.MemberProfileIds
+import javax.persistence.*
 
 @Entity
+
 class LearningHistory(
 
 
     // 값타입. 값타입을 정의하는 클래스를 VO라고 한다. Embedded
 
-    // 회원 정보 (profile Id, member Id)
-    // memberProfileId VO
+    // 회원 정보 (profile Id, member Id가 MemberProfileIds 클래스에 존재)
+    @Embedded
+    @AttributeOverrides(
+        AttributeOverride(name = "memberId", column = Column(name = "member_id", nullable = false)),
+        AttributeOverride(name = "profileId", column = Column(name = "profile_id", nullable = false)),
+    ) // VO 내 컬럼명이 중복되므로 매핑정보 재정의
+    var memberProfileIds: MemberProfileIds
 
     // 컨텐츠 구분 (ActType, ContentType)
     // ContentClassification VO
