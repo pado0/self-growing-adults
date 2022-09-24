bc1d22a1404cec654dddb02ed0a9d4ec4e0a150f
pado0 <bgshhd95@gmail.com>
2022-09-24 11:59:03 +0900

    learning score 작업시작, adapter 패턴 적용해보기

--- domain/src/main/kotlin/entity/LearningScore.kt
+++ domain/src/main/kotlin/entity/LearningScore.kt
@@ -1,4 +1,19 @@
 package entity
 
-class LearningScore {
+import constant.DB_NULL_ID
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class LearningScore(
+
+
+
+    // LearningHistory와 다대일 단방향 연관관계 맵핑
+
+) {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = DB_NULL_ID
 }
